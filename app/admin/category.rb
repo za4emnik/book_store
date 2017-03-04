6e66acbdff9ceff78881eecc64d5e1bd68aafb1f
za4emnik <maxots@rambler.ru>
2017-03-04 12:33:14 +0200

    add batch actions

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Category do
+  config.batch_actions = true
   permit_params :name
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    actions
+  end
+
+  actions :all
+
+  batch_action :destroy do |ids|
+    redirect_to collection_path, alert: "Didn't really delete these!"
+  end
 end
