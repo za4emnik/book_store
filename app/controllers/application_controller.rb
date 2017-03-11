0eefa2fb4f5cee8af684b11468e2bbfc1d18304d
za4emnik <maxots@rambler.ru>
2017-03-11 11:53:33 +0200

    change ability for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  #def access_denied(exception)
-  #  redirect_to admin_organizations_path, alert: exception.message
-  #end
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
 
   def after_sign_in_path_for(resource)
     resource.is_admin? ? admin_root_path : root_path
