ccdb436a16bfb13ceb268b72f3c409116d0ab21c
za4emnik <maxots@rambler.ru>
2017-10-09 10:39:59 +0300

    deploy to digitalocean

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,3 @@
-# Load the Rails application.
 require_relative 'application'
-#require "haml"
-#require 'haml/template/plugin'
 
-# Initialize the Rails application.
 Rails.application.initialize!
-#ActionMailer::Base.register_template_extension('haml')
