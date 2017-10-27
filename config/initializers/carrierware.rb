80c8ef6e68f4361c6f94ca7313da883ff3867217
za4emnik <maxots@rambler.ru>
2017-10-27 15:43:32 +0300

    fix carrierware initializer

--- config/initializers/carrierware.rb
+++ config/initializers/carrierware.rb
@@ -1,25 +1,21 @@
-CarrierWave.configure do |config|
-
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
   end
+end
 
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION']
+    }
+    config.storage = :fog
+    config.fog_directory  = ENV['AWS_FOG_DIRECTORY']
+    config.fog_public     = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
   end
-
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['AWS_REGION']
-  }
-  config.fog_directory  = ENV['AWS_FOG_DIRECTORY']
-  config.fog_public     = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
 end
